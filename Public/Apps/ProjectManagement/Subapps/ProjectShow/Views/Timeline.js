5d6af9b3c8f0b32792d9f17ca3d385716d8760ce
Antoine Jackson <a.j.william26@gmail.com>
2013-12-01 21:54:47 +0100

    ~ Made timeline handle tasks

--- Public/Apps/ProjectManagement/Subapps/ProjectShow/Views/Timeline.js
+++ Public/Apps/ProjectManagement/Subapps/ProjectShow/Views/Timeline.js
@@ -10,7 +10,7 @@ define([
         initialize: function () {
             var base = this;
         },
-        init: function () {
+        init: function (tasks) {
             var base = this;
 
             base.render();
@@ -18,6 +18,7 @@ define([
             base.moving = false;
             base.lastx = undefined;
             base.current_trans = 0;
+            base.tasks = tasks;
         },
         render: function () {
             var base = this;
@@ -30,12 +31,10 @@ define([
             base.$canvas.attr("height", base.$el.height());
             base.canvas = base.$canvas[0];
 
-            var info = base.model.getTimeInfo();
-            console.log(info);
             base.update();
         },
         update: function () {
-            var base = this
+            var base = this;
             var canvas = base.canvas;
 
             base.$canvas.attr("width", base.$el.width());
@@ -44,7 +43,7 @@ define([
             var ctx = canvas.getContext('2d');
 
             var now = new Date();
-//            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
 
             var rect_width = 60;
             var margin_width = 10;
@@ -60,7 +59,8 @@ define([
             } else {
                 base.current_trans *= 0.9;
             }
-            ctx.translate(base.current_trans + canvas.width / 2 - rect_width /2, 0);
+            ctx.translate(base.current_trans + canvas.width / 2 - rect_width / 2, 0);
+
 
             for (var i = -30; i <= 30; i++) {
                 var day = new Date(now);
@@ -68,11 +68,26 @@ define([
                 var day_start = new Date(day);
                 day_start.setHours(0, 0, 0);
                 var day_end = new Date(day);
-                day_end.setHours(23,59,59);
-                var info = base.model.getTimeInfo(day_start, day_end);
+                day_end.setHours(23, 59, 59);
+
+                var cumulated_total = 0;
+                var cumulated_left = 0;
+                var cumulated_done = 0;
+
+                for (var k in base.tasks) {
+                    var task = base.tasks[k];
+                    cumulated_total += task.getDuration(day_start, day_end)
+                    cumulated_left += task.getLeftDuration(day_start, day_end)
+                    cumulated_done += task.getDoneDuration(day_start, day_end)
+                }
+                var info = {
+                    total: cumulated_total,
+                    done: cumulated_done,
+                    left: cumulated_left
+                };
+
                 ctx.fillStyle = "rgba(255,255,255,0.3)";
                 ctx.fillRect(i * rect_width + i * margin_width, 0, rect_width, canvas.height - 20);
-
 
 
                 var height = info.total * hour_height / 3600000;
@@ -87,7 +102,7 @@ define([
                 var tmes = ctx.measureText(text);
                 ctx.fillText(text, (i * rect_width + i * margin_width) + (rect_width - tmes.width) / 2, canvas.height - height + 10 - 20);
                 ctx.fillStyle = "black";
-                var text = day.getMonthName() + day.getDate();
+                var text = day.getMonthName() + " " + day.getDate();
                 var tmes = ctx.measureText(text);
                 ctx.fillText(text, (i * rect_width + i * margin_width) + (rect_width - tmes.width) / 2, canvas.height);
             }
@@ -96,6 +111,14 @@ define([
             requestAnimationFrame(function () {
                 base.update();
             }, base.canvas);
+        },
+        setTasks: function (tasks) {
+            var base = this;
+            if (tasks.models) {
+                base.tasks = tasks.models;
+            } else {
+                base.tasks = tasks;
+            }
         },
         registerEvents: function () {
             var base = this;
